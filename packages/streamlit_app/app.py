8f21e670e3225522ce831167cc44800a3a1b6c85
Zach Parent <zachparent@duck.com>
2025-12-03 11:45:21 -0500

    add streamlit app package

---
+++ packages/streamlit_app/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="WetNet", page_icon="👋", layout="wide")
+
+st.title("👋 Welcome to WetNet!")
+st.write("This is a simple Streamlit application.")
