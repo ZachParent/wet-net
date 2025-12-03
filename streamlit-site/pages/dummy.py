f07a9886ae47c964932f412043a8e3f9c203318d
Zach Parent <zachparent@duck.com>
2025-12-03 12:20:40 -0500

    move streamlit site out of packages

---
+++ streamlit-site/pages/dummy.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import pandas as pd
+import streamlit as st
+
+from wet_net.paths import RESULTS_DIR
+
+results_path = RESULTS_DIR / "dummy_results.csv"
+
+df = pd.read_csv(results_path)
+
+st.title("Dummy Results Viewer")
+st.dataframe(df)
